64289c6a10152b349b0f16dcee63d4e3f7c7cba6
brian7346 <doctorbrian97@gmail.com>
2025-05-21 23:15:01 +0400

    first commit

---
+++ src/01-basic/01-state/src/App.01-state.problem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Задание 01-state
+
+function App() {
+  return (
+    <div>
+      <h1>Задание 01-state</h1>
+      <p>Добавьте ваш React-код здесь</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/02-advanced/01-type-props/src/App.01-type-props.problem.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Задание 01-type-props
+
+function App(): React.ReactElement {
+  return (
+    <div>
+      <h1>Задание 01-type-props</h1>
+      <p>Добавьте ваш React-код здесь</p>
+    </div>
+  );
+}
+
+export default App;
